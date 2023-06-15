1f4712cbfa3cac49bb704448d8a30c6aad74861c
Samir <samir.islam@accenture.com>
2023-06-15 09:00:50 +0200

    add test method from methods class

---
+++ DateiRenamer/Methods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateiRenamer
+{
+    internal class Methods
+    {
+        public static string test()
+        {
+            return "test";
+        }
+    }
+}
