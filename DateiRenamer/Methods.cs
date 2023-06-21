6f52875da8f506dcf3977c9e4d01c89f57f3a8ce
Samir <samir.islam@accenture.com>
2023-06-21 16:28:14 +0200

    add prefix changing function and sub-directory processing and comments

--- DateiRenamer/Methods.cs
+++ DateiRenamer/Methods.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,27 +10,34 @@ namespace DateiRenamer
     internal class Methods
     {
         /// <summary>
-        /// Returns a directory path from user input and lists all files in directory
+        /// Returns a directory path from user input and lists all files in directory and subdirectories
         /// </summary>
         public static string getDirectory()
         {
             Console.WriteLine("Bitte den Pfad zu dem Ordner angeben, in dem die Dateien umbenannt werden sollen:");
 
-            string directoryPath = Console.ReadLine()!;
+            // Get directory path from user input:
+            string directoryPath = Console.ReadLine()!; // The `!` removes the "can be null" warning
             Boolean validityCheck = false;
 
+            // Get new user input if previous input was invalid:
             while (validityCheck == false)
             {
-                if (System.IO.Directory.Exists(directoryPath))
+                if (System.IO.Directory.Exists(directoryPath)) // Check if path exists
                 {
-                    validityCheck = true;
+                    validityCheck = true; // Stop the loop because user input is valid
                     Console.WriteLine($"Pfad existiert");
                     Console.WriteLine($"Dateien in Pfad {directoryPath}:");
 
+                    // Print all file names in the console:
                     foreach (string filePath in Directory.GetFiles(directoryPath))
                     {
                         Console.WriteLine(Path.GetFileName(filePath));
                     }
+
+                    //Recursively process subdirectories:
+                    Console.WriteLine($"Unterordner in Pfad {directoryPath}:");
+                    subdirectoryProcessor(new DirectoryInfo(directoryPath));
                 }
                 else
                 {
@@ -39,6 +47,71 @@ namespace DateiRenamer
             }
 
             return directoryPath;
+        }
+
+        private static void subdirectoryProcessor(DirectoryInfo directory)
+        {
+            DirectoryInfo[] subDirectories = directory.GetDirectories(); // `DirectoryInfo` provides methods to work with directories
+
+            foreach (DirectoryInfo subDirectory in subDirectories)
+            {
+                Console.WriteLine(subDirectory.FullName + ":");
+
+                // Print all file names in the subdirectory:
+                foreach (string filePath in Directory.GetFiles(subDirectory.FullName))
+                {
+                    Console.WriteLine(Path.GetFileName(filePath));
+                }
+
+                // Recursively process subdirectories of the subdirectory:
+                subdirectoryProcessor(subDirectory);
+            }
+        }
+
+        public static void changePrefix(string directoryPath)
+        {
+            Console.WriteLine("Präfix eingeben, der ersetzt werden soll (leer lassen wenn nur Präfix hinzugefügt werden soll):");
+            string oldPrefix = Console.ReadLine()!; // Get old prefix from user input
+            Console.WriteLine("neuen Präfix eingeben:");
+            string newPrefix = Console.ReadLine()!; // Get new prefix from user input
+
+            if (string.IsNullOrEmpty(newPrefix))
+            {
+                Console.WriteLine("Keinen neuen Präfix angegeben...");
+                return;
+            }
+
+            prefixProcessor(new DirectoryInfo(directoryPath), oldPrefix, newPrefix); // Processing the prefix changing in a separate function for recursion purposes. If we added recursion to this function, we would have to enter new prefixes every single time
+        }
+
+        private static void prefixProcessor(DirectoryInfo directory, string oldPrefix, string newPrefix)
+        {
+            FileInfo[] files = directory.GetFiles(); // `FileInfo[]` provides methods to work with files
+
+            foreach (FileInfo file in files)
+            {
+                string oldFileName = file.Name;
+                string newFileName;
+
+                if (!string.IsNullOrEmpty(oldPrefix) && oldFileName.StartsWith(oldPrefix))
+                {
+                    newFileName = oldFileName.Replace(oldPrefix, newPrefix); // REMINDER COMMENT: The string.Replace() function could be used to replace any part of the file name, not just the prefix
+                }
+                else
+                {
+                    newFileName = newPrefix + oldFileName;
+                }
+
+                string newPath = Path.Combine(directory.FullName, newFileName);
+                file.MoveTo(newPath);
+            }
+
+            //Create recursion for sub-directory processing:
+            DirectoryInfo[] subDirectories = directory.GetDirectories();
+            foreach (DirectoryInfo subDirectory in subDirectories)
+            {
+                prefixProcessor(subDirectory, oldPrefix, newPrefix);
+            }
         }
     }
 }
--- DateiRenamer/Program.cs
+++ DateiRenamer/Program.cs
@@ -9,10 +9,17 @@
             Console.WriteLine("Auswählen, was mit der Datei passieren soll:");
             Console.WriteLine("[0]: Präfix ändern, [1]: Suffix ändern");
 
-            int option;
-            bool optionCheck = int.TryParse(Console.ReadLine(), out option);
+            int option; // The option chosen by the user is stored in the option variable
+            bool optionCheck = int.TryParse(Console.ReadLine(), out option); // TryParse returns a false boolean if the user enters invalid input which makes error handling easier
 
-            while (optionCheck == false)
+            // Set the check boolean to false if the user enters an option that doesn't exist:
+            if (option > 1)
+            {
+                optionCheck = false;
+            }
+
+            // Get new user input if previous input was invalid:
+            while (optionCheck == false) 
             {
                 Console.WriteLine("Ungültige Option, nochmal versuchen:");
                 optionCheck = int.TryParse(Console.ReadLine(), out option);
@@ -21,6 +28,16 @@
                 {
                     optionCheck = false;
                 }
+            }
+
+            // Process chosen option:
+            switch(option)
+            {
+                case 0:
+                    Methods.changePrefix(path);
+                    break;
+                case 1:
+                    break;
             }
         }
     }
